f176206b592b4774533ca0506ad9269dbc48f83a
97morningstar <97morningstar@gmail.com>
2017-06-22 17:16:45 -0500

    se acyualizo

--- formulario/script.js
+++ formulario/script.js
@@ -4,6 +4,12 @@ var nombre = document.getElementById('names');
 var apel = document.getElementById('apel');
 var edad = document.getElementById('edad');
 var divo = document.getElementById("datos");
+
+var valor1 = nombre.value;
+var valor2 = apel.value;
+var valor3 = edad.value;
+
+
 var cosa1 = document.createElement('p');
 var cosa2 = document.createElement('p');
 var cosa3 = document.createElement('p');
@@ -12,39 +18,54 @@ var texto2;
 var texto3;
 
 function uno(){
-
-texto1 = "El nombre es: " + nombre.value;
-texto2 = "El apellido es: " + apel.value;
-texto3 = "La edad es: " + edad.value;
-
+valor3 = edad.value;
+valor2 = apel.value;
+valor1 = nombre.value;
 
 
-/*
-texto1 = document.createTextNode("El nombre es: " + nombre.value);
-texto2 = document.createTextNode("El apellido es: " + apel.value);
-texto3 = document.createTextNode("La edad es: " + edad.value);
-*/
+	if( /\d{1,}/g.test(valor3)){
+		if(/[a-z/*-+]{1,}/g.test(valor3)){  //faltan simbolos
+			texto3 = "Tu edad contiene letras o simbolos?";	
+		}else{
+		 	texto3 = "La edad es: " + edad.value;
+		}
+	}
+	else {
+		texto3 = "No has introducido numeros en la edad";
+	}
+
+
+if(/[a-z]/ig.test(valor1)){
+  if(/\d{1,}/g.test(valor1))
+  {
+  	texto1 = "Numeros no permitidos en el nombre o cualquier otro caracter que no sea letras";
+  }else{
+	texto1 = "El nombre es: " + nombre.value;
+}
+}else{
+	texto1 = "Numeros no permitidos en el nombre o cualquier otro caracter que no sea letras";
+}
+
+if(/[a-z]/ig.test(valor2)){
+  if(/\d{1,}/g.test(valor2))
+  {
+  	texto2 = "Numeros no permitidos en el apellido o cualquier otro caracter que no sea letras";
+  }else{
+	texto2 = "El apellido es: " + apel.value;
+}
+}else{
+	texto2 = "Numeros no permitidos en el apellido o cualquier otro caracter que no sea letras";
+}
+
 }
 
 function Assignar(){
-
 cosa1.textContent = texto1;
-
-//cosa1.appendChild(texto1);
-
 divo.appendChild(cosa1);
-
 cosa2.textContent = texto2;
-
-//cosa2.appendChild(texto2);
 divo.appendChild(cosa2);
-
 cosa3.textContent = texto3;
-
-//cosa3.appendChild(texto3);
 divo.appendChild(cosa3);
-
-
 }
 
 var di = document.getElementById("boton");
@@ -52,8 +73,6 @@ if(di.addEventListener){
 	di.addEventListener("click", function(){
         uno();
 		Assignar();
-
-		
 	}, false);
 }
 	
