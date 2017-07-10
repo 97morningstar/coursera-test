5eef9c4f787cd101c2be4dbf4f7b07e2e82344bf
97morningstar <97morningstar@gmail.com>
2017-07-10 01:27:55 -0500

    actualize el main.js del blog del mar

--- Blog_del_mar/js/main.js
+++ Blog_del_mar/js/main.js
@@ -57,3 +57,19 @@ if(isOpen){
 	init();
 
 })();
+
+
+	$(window).scroll(function(){
+	    if ($(this).scrollTop() > 300) {
+	      $('.scrollToTop').fadeIn();
+	    } else {
+	      $('.scrollToTop').fadeOut();
+	    }
+	});
+
+	//Click event to scroll to top
+
+	$('.scrollToTop').click(function(){
+	    $('html, body').animate({scrollTop : 0},800);
+	    return false;
+	});
